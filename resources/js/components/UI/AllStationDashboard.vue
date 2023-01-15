f45800815ede73fdf76508b0cd27ed208d952783
Arkadiusz Torba <suabyak@gmail.com>
2023-01-15 10:14:28 +0100

    Stations.json update

--- resources/js/components/UI/AllStationDashboard.vue
+++ resources/js/components/UI/AllStationDashboard.vue
@@ -7,7 +7,7 @@
         class="flex flex-col my-4 p-3 bg-gray-800 rounded-lg text-white">
         <div class="flex flex-col space-y-2 break-words">
               <p class="font-semibold lg:text-xl">
-                {{ station.title }}
+                {{ station.name }}
               </p>
         </div>
 
