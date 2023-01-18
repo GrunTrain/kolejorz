33df8e37ab3ac58a54129d56a89524b3d448ddbf
KozaPawel <p.koza99@o2.pl>
2023-01-18 17:51:16 +0100

    #62 - change 404 page

--- resources/js/pages/NotFound.vue
+++ resources/js/pages/NotFound.vue
@@ -1,4 +1,21 @@
 <template>
-    <h2>Page not found!</h2>
+    <div class="flex justify-center ">
+        <div class="flex pt-10 px-10 flex-wrap">
+            <img :src="'./thomas.png'">
+            <div class="flex items-center pl-8">
+                <div class="flex flex-col">
+                    <h1 class="flex flex-wrap mx-4 mb-4 mt-10 text-4xl font-bold text-gray-200">Pomyliłeś pociągi?</h1>
+                    <router-link to="/home" class="bg-[#48c1ae] hover:bg-[#32b5a0] text-white font-bold py-2 px-4 rounded flex justify-center">
+                        Wróć na stację główną
+                    </router-link>
+                </div>
+            </div>
+        </div>
+        </div>
 </template>
+
+<script>
+
+</script>
+
 
